05c29929c512e3addb92560c09a0ff6cd8a1dd26
Tamojit Saha <sahatamo1995@gmail.com>
2020-09-29 19:59:29 +0530

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,7 +1,7 @@
-xcopy /s C:\Users\Tamojit\Documents\Arduino\mqtt_esp8266_ota D:\git\esp8266_secure_ota_aws_mqtt
+xcopy /y /s C:\Users\Tamojit\Documents\Arduino\mqtt_esp8266_ota D:\git\esp8266_secure_ota_aws_mqtt
 
 ren mqtt_esp8266_ota.ino esp8266_secure_ota_aws_mqtt.ino
 del mqtt_esp8266_ota.ino
 ren *.bin occu_esp8266.bin
 del mqtt_esp8266_ota.ino.generic.bin
-mv .\occu_esp8266.bin .\fota
+MOVE .\occu_esp8266.bin D:\git\esp8266_secure_ota_aws_mqtt\fota
